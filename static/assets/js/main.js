bcb086494d760cf1c44f36b7f6c495577d28f5cf
Veenah Ganesh <88481243+Veenah07Ganesh@users.noreply.github.com>
2025-06-27 05:55:55 +0800

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,8 +1,4 @@
-/*
-	Massively by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+
 
 (function($) {
 
@@ -14,7 +10,7 @@
 		$main = $('#main'),
 		$navPanelToggle, $navPanel, $navPanelInner;
 
-	// Breakpoints.
+
 		breakpoints({
 			default:   ['1681px',   null       ],
 			xlarge:    ['1281px',   '1680px'   ],
@@ -25,10 +21,7 @@
 			xxsmall:   [null,       '360px'    ]
 		});
 
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
+
 	$.fn._parallax = function(intensity) {
 
 		var	$window = $(window),
